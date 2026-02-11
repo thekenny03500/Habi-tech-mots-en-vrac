a346ff6e81be7f11074ce5faec67bdc524db09a1
Aimeric-Thomas DALVAI <33188191+thekenny03500@users.noreply.github.com>
2026-02-11 08:36:34 +0100

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow, Tray, Menu, nativeImage, ipcMain } = require('electron')
+const path = require('path');
+
+
+let mainWindow;
+const createWindow = () =>
+{
+    mainWindow = new BrowserWindow({
+        frame: true,
+        transparent: false,
+        alwaysOnTop: true,
+        resizable: false,
+        focusable: true,
+        width: 960 + 150,
+        height: 595,
+    })
+
+    mainWindow.loadFile('index.html')
+}
+
+// Demarrage de l'appliation
+app.whenReady().then(() =>
+{
+    createWindow();
+    //mainWindow.webContents.openDevTools({ mode: 'detach' });
+})
