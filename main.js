c486785cd7cabb0b9d3574dce73e73ea2ce05782
Aimeric-Thomas DALVAI <33188191+thekenny03500@users.noreply.github.com>
2026-02-13 14:11:17 +0100

    Correction multiple

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const { app, BrowserWindow } = require('electron')
 
+const CONTENT_WIDTH = 1280;
+const CONTENT_HEIGHT = 720;
+
 let mainWindow;
 const createWindow = () =>
 {
     mainWindow = new BrowserWindow({
         frame: true,
-        transparent: false,
-        alwaysOnTop: true,
         resizable: false,
-        focusable: true,
-        width: 1280 + 150,
-        height: 720 + 52,
+        backgroundColor:"#1d274f",
     })
-
+    mainWindow.setMenuBarVisibility(false);
     mainWindow.loadFile('index.html')
+    // ← Force la taille du contenu APRÈS le chargement
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.setContentSize(CONTENT_WIDTH+150, CONTENT_HEIGHT);
+    });
 }
 
 // Demarrage de l'appliation
 app.whenReady().then(() =>
 {
     createWindow();
+    //mainWindow.webContents.openDevTools();
 })
